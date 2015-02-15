391be7a65f77bd6c55eff9f9391c92960058c6ea
Quintus <quintus@quintilianus.eu>
2015-02-15 23:16:55 +0100

    Draw chessboard background on editor widget

--- src/graphics_editor.cpp
+++ src/graphics_editor.cpp
@@ -21,6 +21,29 @@ void GraphicsEditor::on_paint(wxPaintEvent& evt)
   if (!p_gc)
     return;
 
+  // Draw a chessboard pattern over the entire area as the background
+  wxSize dimensions = GetClientSize();
+  wxGraphicsPath grey_path = p_gc->CreatePath();
+  wxGraphicsPath white_path = p_gc->CreatePath();
+  int x = 0;
+  int y = 0;
+  bool white = true;
+  for(x = 0; x < dimensions.GetWidth(); x += 16) {
+    for(y = 0; y < dimensions.GetHeight(); y += 16) {
+      if (white)
+	white_path.AddRectangle(x, y, 16, 16);
+      else
+	grey_path.AddRectangle(x, y, 16, 16);
+
+      white = !white;
+    }
+  }
+
+  p_gc->SetBrush(*wxWHITE_BRUSH);
+  p_gc->FillPath(white_path);
+  p_gc->SetBrush(*wxGREY_BRUSH);
+  p_gc->FillPath(grey_path);
+
   p_gc->SetPen(*wxBLACK_PEN);
   p_gc->StrokeLine(0, 0, 200, 200);
 
